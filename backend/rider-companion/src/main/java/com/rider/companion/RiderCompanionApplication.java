340a7156a4259677bf330a41735f59c828784650
OuSsAmA <oussama.bouhastine@esprit.tn>
2026-07-21 14:09:26 +0100

    feat: initialize spring boot backend

---
+++ backend/rider-companion/src/main/java/com/rider/companion/RiderCompanionApplication.java
@@ -0,0 +1,13 @@
+package com.rider.companion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiderCompanionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiderCompanionApplication.class, args);
+	}
+
+}
